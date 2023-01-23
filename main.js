fb0d085d95016a3814cb7b997d54080010bc0dfb
MammothSui <123360827+MammothSui@users.noreply.github.com>
2023-01-23 14:29:00 +0530

    uploaded

---
+++ main.js
@@ -0,0 +1,26 @@
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+
+
+
+function showImage(){
+    document.getElementById('image').style.display = 'block';
+    document.getElementById('showImage').style.display = 'none';
+}
